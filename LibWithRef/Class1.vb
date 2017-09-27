c25fddb552f4992a52b52936c9698b655e517a6d
Jan Suchotzki <jan@lernmoment.de>
2017-09-27 15:59:37 +0200

    Adds example showing problem

---
+++ LibWithRef/Class1.vb
@@ -0,0 +1,13 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.Structure
+
+Public Class Class1
+
+    Public Shared Sub CallToNuGetPackage()
+        Dim img As New Image(Of Bgr, Byte)("FragLernMoment.png")
+
+        'Show the image
+        UI.ImageViewer.Show(img)
+    End Sub
+
+End Class
--- NuGetRefTest/Module1.vb
+++ NuGetRefTest/Module1.vb
@@ -1,7 +1,9 @@
-﻿Module Module1
+﻿Imports LibWithRef
+
+Module Module1
 
     Sub Main()
-
+        Class1.CallToNuGetPackage()
     End Sub
 
 End Module
